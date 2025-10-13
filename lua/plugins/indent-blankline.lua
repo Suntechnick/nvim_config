9713f863ed54c69d13bd737b35ca288212c31254
Suntechnick <troxybbax@gmail.com>
2025-10-13 15:23:53 +0300

    Update indent-blankline.lua

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,6 +1,8 @@
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	config = function()
-		require("indent_blankline").setup()
-	end,
+        "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {}
+        -- config = function()
+        --      require("indent_blankline").setup()
+        -- end,
 }
