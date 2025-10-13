94aedc1d226a9e5fadcb89da9439dd7bbe413624
Suntechnick <troxybbax@gmail.com>
2025-10-13 15:26:06 +0300

    Update wk-mappings.lua

--- lua/suntechnick/wk-mappings.lua
+++ lua/suntechnick/wk-mappings.lua
@@ -1,57 +1,28 @@
 local wk = require("which-key")
 
-wk.register({
-	f = {
-		name = "Find",
-		f = { "<cmd>Telescope find_files<cr>", "Find file" },
-		w = { "<cmd>Telescope live_grep<cr>", "Find word" },
-	},
-	c = { "<cmd>BufferLinePickClose<cr>", "Pick and close the opened buffer" },
-	l = {
-		name = "LSP",
-		a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code action" },
-		r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename symbol" },
-		d = { "<cmd>lua vim.diagnostic.open_float({source = true})<cr>", "Line diagnostics" },
-		D = { "<cmd>Telescope diagnostics<cr>", "File diagnostics"}
-	},
-	n = {
-		name = "Plugins",
-		p = { "<cmd>NoNeckPain<cr>", "No neck pain" },
-	},
-	["/"] = {
-		function()
-			require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)
-		end,
-		"Toggle comment line",
-	},
-}, { prefix = "<leader>" })
-
-wk.register({
-	["["] = {
-		b = { "<cmd>bprevious<cr>", "Previous buffer" },
-	},
-
-	["]"] = {
-		b = { "<cmd>bnext<cr>", "Next buffer" },
-	},
-	["<C-h>"] = { "<C-w>h", "Previous window" },
-	["<C-l>"] = { "<C-w>l", "Next window" },
-	s = {
-		function()
-			require("leap").leap({ target_windows = { vim.fn.win_getid() } })
-		end,
-		"Leap",
-	},
-	["<C-d>"] = { "<C-d>zz", "Scroll down and center the view" },
-	["<C-u>"] = { "<C-u>zz", "Scroll up and center the view" },
-	g = {
-		d = { "<cmd>lua vim.lsp.buf.definition()<cr>", "Go to definition" },
-	},
+wk.add({
+  {
+    "<leader>/",
+    "<cmd>lua require('Comment.api').toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)<cr>",
+    desc = "Toggle comment line"
+  },
+  { "<leader>c",  "<cmd>BufferLinePickClose<cr>",                                                  desc = "Pick and close the opened buffer" },
+  { "<leader>f",  group = "Find" },
+  { "<leader>ff", "<cmd>Telescope find_files<cr>",                                                 desc = "Find file" },
+  { "<leader>fw", "<cmd>Telescope live_grep<cr>",                                                  desc = "Find word" },
+  { "<leader>l",  group = "LSP" },
+  { "<leader>lD", "<cmd>Telescope diagnostics<cr>",                                                desc = "File diagnostics" },
+  { "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>",                                        desc = "Code action" },
+  { "<leader>ld", "<cmd>lua vim.diagnostic.open_float({source = true})<cr>",                       desc = "Line diagnostics" },
+  { "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>",                                             desc = "Rename symbol" },
+  { "<leader>n",  group = "Plugins" },
+  { "<leader>np", "<cmd>NoNeckPain<cr>",                                                           desc = "No neck pain" },
+  { "<C-d>",      "<C-d>zz",                                                                       desc = "Scroll down and center the view" },
+  { "<C-h>",      "<C-w>h",                                                                        desc = "Previous window" },
+  { "<C-l>",      "<C-w>l",                                                                        desc = "Next window" },
+  { "<C-u>",      "<C-u>zz",                                                                       desc = "Scroll up and center the view" },
+  { "[b",         "<cmd>bprevious<cr>",                                                            desc = "Previous buffer" },
+  { "]b",         "<cmd>bnext<cr>",                                                                desc = "Next buffer" },
+  { "gd",         "<cmd>lua vim.lsp.buf.definition()<cr>",                                         desc = "Go to definition" },
+  { "<leader>/",  "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", desc = "Toggle comment for selection",    mode = "v" },
 })
-
-wk.register({
-	["/"] = {
-		"<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
-		"Toggle comment for selection",
-	},
-}, { prefix = "<leader>", mode = "v" })
